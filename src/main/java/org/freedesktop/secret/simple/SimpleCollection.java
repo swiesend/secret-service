87561f3cabfe28e24b8c48a7162cee847bc8002e
swiesend <sebastian@wiesendahl.de>
2019-01-29 15:18:17 -0600

    add SimpleCollection API

---
+++ src/main/java/org/freedesktop/secret/simple/SimpleCollection.java
@@ -0,0 +1,322 @@
+package org.freedesktop.secret.simple;
+
+import org.freedesktop.dbus.DBusPath;
+import org.freedesktop.dbus.ObjectPath;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.freedesktop.dbus.types.Variant;
+import org.freedesktop.secret.Collection;
+import org.freedesktop.secret.*;
+import org.freedesktop.secret.errors.NoSuchObject;
+import org.gnome.keyring.InternalUnsupportedGuiltRiddenInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.AccessControlException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.*;
+import org.freedesktop.secret.interfaces.Prompt.Completed;
+
+public final class SimpleCollection {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleCollection.class);
+
+    private TransportEncryption encryption = null;
+    private Service service = null;
+    private Session session = null;
+    private Prompt prompt = null;
+    private InternalUnsupportedGuiltRiddenInterface withoutPrompt = null;
+
+    private Collection collection;
+    private List<ObjectPath> unlock = null;
+
+    /**
+     * The default collection.
+     */
+    public SimpleCollection() {
+        init();
+        ObjectPath path = Static.Convert.toObjectPath(Static.ObjectPaths.DEFAULT_COLLECTION);
+        this.collection = new Collection(path, service);
+        unlock(null);
+    }
+
+    /**
+     * A user specified collection.
+     *
+     * @param label     The label of the collection.
+     *
+     *                  NOTE: The 'label' of a collection may differ from the 'id' of a collection, which get assigned
+     *                  by the Secret Service.
+     *
+     *                  A SimpleCollection can't handle collections with the same label, but different ids correctly.
+     */
+    public SimpleCollection(String label, String password) {
+        init();
+
+        if (exists(label)) {
+            ObjectPath path = getCollectionPath(label);
+            this.collection = new Collection(path, service);
+        } else {
+            Map<String, Variant> properties = Collection.createProperties(label);
+            DBusPath path = null;
+
+            if (password == null) {
+                Pair<ObjectPath, ObjectPath> response = service.createCollection(properties);
+                if (!response.a.getPath().equals("/")) {
+                    path = response.a;
+                }
+                performPrompt(response.b);
+            } else {
+                Secret encrypted = null;
+                try {
+                    encrypted = encryption.encrypt(password);
+                } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+                    log.error(e.toString(), e.getCause());
+                }
+                path = withoutPrompt.createWithMasterPassword(properties, encrypted);
+            }
+
+            if (path == null) {
+                try {
+                    Thread.sleep(100L);
+                } catch (InterruptedException e) {
+                    log.error(e.toString(), e.getCause());
+                }
+                Service.CollectionCreated cc = (Service.CollectionCreated) service.getSignalHandler().getLastHandledSignal();
+                path = cc.collection;
+            }
+
+            this.collection = new Collection(path, service);
+        }
+
+        service.lock(Arrays.asList(collection.getPath()));
+        unlock(password);
+    }
+
+    private void init() {
+        try {
+            encryption = new TransportEncryption();
+            encryption.initialize();
+            encryption.openSession();
+            encryption.generateSessionKey();
+            service = encryption.getService();
+            session = service.getSession();
+            prompt = new Prompt(service);
+            withoutPrompt = new InternalUnsupportedGuiltRiddenInterface(service);
+        } catch (DBusException | NoSuchAlgorithmException | InvalidAlgorithmParameterException | InvalidKeySpecException | InvalidKeyException e) {
+            log.error(e.toString(), e.getCause());
+        }
+    }
+
+    private Map<ObjectPath, String> getLabels() {
+        List<ObjectPath> collections = service.getCollections();
+
+        Map<ObjectPath, String> labels = new HashMap();
+        for (ObjectPath path: collections) {
+            Collection c = new Collection(path, service, null);
+            labels.put(path, c.getLabel());
+        }
+
+        return labels;
+    }
+
+    private boolean exists(String label) {
+        Map<ObjectPath, String> labels = getLabels();
+        return labels.values().contains(label);
+    }
+
+    private ObjectPath getCollectionPath(String label) {
+        Map<ObjectPath, String> labels = getLabels();
+
+        ObjectPath path = null;
+        for (Map.Entry<ObjectPath, String> entry: labels.entrySet()) {
+            ObjectPath p = entry.getKey();
+            String l = entry.getValue();
+            if (l.equals(label)) {
+                path = p;
+                break;
+            }
+        }
+        return path;
+    }
+
+    private final boolean isDefault() {
+        List<String> defaults = Arrays.asList(null, "login", "session", "default");
+        return defaults.contains(collection.getId());
+    }
+
+    private void performPrompt(ObjectPath path) {
+        if (!path.getPath().equals("/")) {
+            try {
+                prompt.await(path);
+            } catch (InterruptedException | NoSuchObject e) {
+                log.error(e.toString(), e.getCause());
+            }
+        }
+    }
+
+    private void unlock(String password) {
+        if (unlock == null) {
+            unlock = Arrays.asList(collection.getPath());
+        }
+        if (collection.isLocked()) {
+            if (password == null) {
+                Pair<List<ObjectPath>, ObjectPath> response = service.unlock(unlock);
+                performPrompt(response.b);
+            } else {
+                Secret encrypted = null;
+                try {
+                    encrypted = encryption.encrypt(password);
+                } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+                    log.error(e.toString(), e.getCause());
+                }
+                withoutPrompt.unlockWithMasterPassword(collection.getPath(), encrypted);
+            }
+        }
+    }
+
+    private Item pathToItem(DBusPath path) {
+        return new Item(Static.Convert.toObjectPath(path.getPath()), service);
+    }
+
+    private void getUserPermission() {
+        if(isDefault()) {
+            List<ObjectPath> lockable = Arrays.asList(collection.getPath());
+            service.lock(lockable);
+            try {
+                Thread.sleep(250L);
+            } catch (InterruptedException e) {
+                log.error(e.toString(), e.getCause());
+            }
+            Pair<List<ObjectPath>, ObjectPath> response = service.unlock(lockable);
+            performPrompt(response.b);
+            if (collection.isLocked()) {
+                throw new AccessControlException("One may not read all passwords from a default collection without permission.");
+            }
+        }
+    }
+
+    /**
+     * Delete this collection.
+     */
+    public void delete() throws AccessControlException {
+        if (!isDefault()) {
+            ObjectPath promptPath = collection.delete();
+            performPrompt(promptPath);
+        } else {
+            throw new AccessControlException("Default collections may not be deleted with the simple API.");
+        }
+    }
+
+    public DBusPath createPassword(String label, String password, Map<String, String> attributes) throws IllegalArgumentException {
+
+        if (password == null) {
+            throw new IllegalArgumentException("The password may not be null.");
+        }
+        if (label == null) {
+            throw new IllegalArgumentException("The label of the password may not be null.");
+        }
+
+        DBusPath item = null;
+        try {
+            //unlock(null);
+            Map<String, Variant> properties = Item.createProperties(label, attributes);
+            Secret secret = encryption.encrypt(password);
+            Pair<ObjectPath, ObjectPath> response = collection.createItem(properties, secret, false);
+            performPrompt(response.b);
+            item = response.a;
+            if (item.equals("/")) {
+                Completed completed = prompt.getLastHandledSignal();
+                if (!completed.dismissed) {
+                    Collection.ItemCreated ic = (Collection.ItemCreated) collection.getSignalHandler().getLastHandledSignal();
+                    item = ic.item;
+                }
+            }
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException  e) {
+            log.error(e.toString(), e.getCause());
+        }
+
+        return item;
+    }
+
+    public DBusPath createPassword(String label, String password) throws IllegalArgumentException {
+        return createPassword(label, password, null);
+    }
+
+    public void updatePassword(DBusPath object, String label, String password, Map<String, String> attributes) throws IllegalArgumentException {
+
+        if (object == null) {
+            throw new IllegalArgumentException("The object path of the item may not be null.");
+        }
+        if (password == null) {
+            throw new IllegalArgumentException("The new password may not be null.");
+        }
+        if (label == null) {
+            throw new IllegalArgumentException("The new label of the item may not be null.");
+        }
+
+        try {
+            unlock(null);
+            Item item = pathToItem(object);
+            item.setLabel(label);
+            if (attributes == null) {
+                item.setAttributes(Collections.emptyMap());
+            } else {
+                item.setAttributes(attributes);
+            }
+            Secret secret = encryption.encrypt(password);
+            item.setSecret(secret);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            log.error(e.toString(), e.getCause());
+        }
+    }
+
+    public Item getItem(DBusPath object) {
+        unlock(null);
+        return pathToItem(object);
+    }
+
+    public String getPassword(DBusPath object) {
+        unlock(null);
+        Item item = pathToItem(object);
+        Secret secret = item.getSecret(session.getPath());
+        String decrypted = null;
+        try {
+            decrypted = encryption.decrypt(secret);
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            log.error(e.toString(), e.getCause());
+        }
+        return decrypted;
+    }
+
+    public Map<DBusPath, String> getPasswords() {
+        getUserPermission();
+
+        List<ObjectPath> items = collection.getItems();
+        Map<DBusPath, String> passwords = new HashMap();
+        for (ObjectPath item : items) {
+            passwords.put(item, getPassword(item));
+        }
+        return passwords;
+    }
+
+    public void deletePassword(DBusPath object) {
+        getUserPermission();
+
+        Item item = pathToItem(object);
+        ObjectPath promptPath = item.delete();
+        performPrompt(promptPath);
+    }
+
+    public void deletePasswords(List<DBusPath> objects) {
+        for (DBusPath item : objects) {
+            deletePassword(item);
+        }
+    }
+
+}
---
+++ src/test/java/org/freedesktop/secret/simple/SimpleCollectionTest.java
@@ -0,0 +1,153 @@
+package org.freedesktop.secret.simple;
+
+import org.freedesktop.dbus.DBusPath;
+import org.freedesktop.secret.interfaces.Item;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.AccessControlException;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCollectionTest {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleCollectionTest.class);
+
+    private String getRandomHexString(int length){
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer();
+        while(sb.length() < length){
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString();
+    }
+
+    @Test
+    @Disabled
+    void deleteDefaultCollection() {
+        SimpleCollection defaultCollection = new SimpleCollection();
+        assertThrows(AccessControlException.class, () -> defaultCollection.delete());
+    }
+
+    @Test
+    void deleteNonDefaultCollection() {
+        SimpleCollection collection = new SimpleCollection("test", "test");
+        assertDoesNotThrow(() -> collection.delete());
+    }
+
+    @Test
+    void createPassword() {
+        // before all
+        SimpleCollection collection = new SimpleCollection("test", "test");
+
+        DBusPath itemID;
+
+        // 1. create and get password
+        itemID = collection.createPassword("item",  "secret");
+        String actual = collection.getPassword(itemID);
+        assertEquals("secret", actual);
+        Item item = collection.getItem(itemID);
+        assertEquals(Collections.emptyMap(), item.getAttributes());
+        // after 1.
+        collection.deletePassword(itemID);
+
+        // 2. create and get attributes
+        Map<String, String> attributes = new HashMap();
+        String uuid = getRandomHexString(32);
+        attributes.put("uuid", uuid);
+        itemID = collection.createPassword("item", "secret", attributes);
+        item = collection.getItem(itemID);
+        Map<String, String> actualAttributes = item.getAttributes();
+        assertEquals(attributes, actualAttributes);
+        // after 2.
+        collection.deletePassword(itemID);
+
+        // after all
+        collection.delete();
+    }
+
+    @Test
+    void updatePassword() {
+        // before
+        SimpleCollection collection = new SimpleCollection("test", "test");
+        Map<String, String> attributes = new HashMap();
+
+        // create password
+        attributes.put("uuid", getRandomHexString(32));
+        DBusPath itemID = collection.createPassword("item",  "secret", attributes);
+        assertEquals("secret", collection.getPassword(itemID));
+        Item item = collection.getItem(itemID);
+        assertEquals("item", item.getLabel());
+        assertEquals(attributes.get("uuid"), item.getAttributes().get("uuid"));
+
+        // update password
+        attributes.put("uuid", getRandomHexString(32));
+        collection.updatePassword(itemID, "updated item", "updated secret", attributes);
+        assertEquals("updated secret", collection.getPassword(itemID));
+        item = collection.getItem(itemID);
+        assertEquals("updated item", item.getLabel());
+        assertEquals(attributes.get("uuid"), item.getAttributes().get("uuid"));
+
+        // after
+        collection.delete();
+    }
+
+    @Test
+    void getPassword() {
+        // before
+        SimpleCollection collection = new SimpleCollection();
+        DBusPath itemID = collection.createPassword("item", "secret");
+
+        // test
+        String password = collection.getPassword(itemID);
+        assertEquals("secret", password);
+
+        // after
+        collection.deletePassword(itemID);
+    }
+
+    @Test
+    void getPasswordFromNonDefaultCollection() {
+        // before
+        SimpleCollection collection = new SimpleCollection("test", "test");
+
+        // test
+        DBusPath item = collection.createPassword("item","secret");
+        String password = collection.getPassword(item);
+        assertEquals("secret", password);
+
+        // after
+        collection.deletePassword(item);
+        collection.delete();
+    }
+
+    @Test
+    void getPasswords() {
+        SimpleCollection collection = new SimpleCollection();
+        Map<DBusPath, String> passwords = collection.getPasswords();
+        // only with user permission
+        log.info(passwords.toString());
+    }
+
+    @Test
+    void deletePassword() {
+        SimpleCollection collection = new SimpleCollection();
+        DBusPath item = collection.createPassword("item", "secret");
+        // only with user permission
+        collection.deletePassword(item);
+    }
+
+    /**
+     * NOTE: Be aware that this can lead to the loss of passwords if performed on any default collections.
+     */
+    @Test
+    void deletePasswords() {
+        SimpleCollection collection = new SimpleCollection("test", "test");
+        DBusPath item = collection.createPassword("item", "secret");
+        assertDoesNotThrow(() -> collection.deletePasswords(Arrays.asList(item)));
+        collection.delete();
+    }
+}
