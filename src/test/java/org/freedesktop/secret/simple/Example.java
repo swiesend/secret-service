a509e3d63fe0518261f52ca1dc0046639d127a90
swiesend <sebastian@wiesendahl.de>
2019-01-29 15:18:30 -0600

    add SimpleCollection examples

---
+++ src/test/java/org/freedesktop/secret/simple/Example.java
@@ -0,0 +1,57 @@
+package org.freedesktop.secret.simple;
+
+import org.freedesktop.dbus.DBusPath;
+import org.freedesktop.secret.interfaces.Item;
+import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Example {
+
+    @Test
+    @DisplayName("Create a password in the user's default collection ('/org/freedesktop/secrets/aliases/default').")
+    void createPasswordInDefaultCollection() {
+        SimpleCollection collection = new SimpleCollection();
+        DBusPath itemID = collection.createPassword("My Item", "secret");
+        String actual = collection.getPassword(itemID);
+        assertEquals("secret", actual);
+
+        // delete with user's permission trough a prompt.
+        collection.deletePassword(itemID);
+    }
+
+    @Test
+    @DisplayName("Create a password in a non-default collection ('/org/freedesktop/secrets/collection/xxxx').")
+    void createPasswordInNonDefaultCollection() {
+        SimpleCollection collection = new SimpleCollection("My Collection", "super secret");
+        DBusPath itemID = collection.createPassword("My Item", "secret");
+        String actual = collection.getPassword(itemID);
+        assertEquals("secret", actual);
+
+        // delete without prompting, as collection's password is known.
+        collection.deletePassword(itemID);
+        collection.delete();
+    }
+
+    @Test
+    @DisplayName("Create a password with additional attributes.")
+    void createPasswordWithAttributes() {
+        SimpleCollection collection = new SimpleCollection("My Collection", "super secret");
+
+        Map<String, String> attributes = new HashMap();
+        attributes.put("uuid", "42");
+
+        DBusPath itemID = collection.createPassword("My Item", "secret", attributes);
+        String actual = collection.getPassword(itemID);
+        assertEquals("secret", actual);
+        Item item = collection.getItem(itemID);
+        assertEquals("42", item.getAttributes().get("uuid"));
+
+        // delete without prompting, as collection's password is known.
+        collection.deletePassword(itemID);
+        collection.delete();
+    }
+}
