f91fb4e3228115f64b5e60070173c0fb5492360e
swiesend <sebastian@wiesendahl.de>
2019-08-09 15:08:00 +0200

    add documentation for connect()

--- src/main/java/org/freedesktop/secret/simple/SimpleService.java
+++ src/main/java/org/freedesktop/secret/simple/SimpleService.java
@@ -9,6 +9,9 @@ public class SimpleService {
 
     private static final Logger log = LoggerFactory.getLogger(SimpleService.class);
 
+    /**
+     * Connect to the default collection.
+     */
     public Optional<SimpleCollection> connect() {
         try {
             SimpleCollection keyring = new SimpleCollection();
@@ -19,6 +22,20 @@ public class SimpleService {
         }
     }
 
+    /**
+     * Connect to a user specified collection.
+     *
+     * @param label     The displayable label of the collection
+     *
+     *                  <p>
+     *                      NOTE: The 'label' of a collection may differ from the 'id' of a collection. The 'id' is
+     *                      assigned by the Secret Service and used in the DBus object path of a collection or item.
+     *                  <p>
+     *
+     *                  A SimpleCollection can't handle collections with the same label, but different ids correctly.
+     *
+     * @param password  Password of the collection
+     */
     public Optional<SimpleCollection> connect(String label, CharSequence password) {
         try {
             SimpleCollection keyring = new SimpleCollection(label, password);
